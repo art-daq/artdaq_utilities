e0def281784eb338633526ef8d70a60b5ec4fbcf
Kurt Biery <biery@fnal.gov>
2015-03-04 10:27:23 -0600

    added pulls of cetbuildtools versions

--- for_jenkins/build-artdaq.sh
+++ for_jenkins/build-artdaq.sh
@@ -113,6 +113,27 @@ fi
 if [ `ls -1 ${blddir}/artdaq*${dotver}*.tar.bz2 | wc -l` -gt 0 ]; then
   rm -fv ${blddir}/artdaq*${dotver}*.tar.bz2
 fi
+
+# pull cetbuildtools versions specially
+cd ${blddir} || exit 1
+mytar="cetbuildtools-4.05.00-noarch.tar.bz2"
+mydist="http://scisoft.fnal.gov/scisoft/packages/cetbuildtools/v4_05_00/${mytar}"
+echo "pull ${mytar}"
+curl --fail --silent --location --insecure -O ${mydist}
+tar -xf ${mytar}
+cd ${blddir} || exit 1
+mytar="cetbuildtools-4.07.02-noarch.tar.bz2"
+mydist="http://scisoft.fnal.gov/scisoft/packages/cetbuildtools/v4_07_02/${mytar}"
+echo "pull ${mytar}"
+curl --fail --silent --location --insecure -O ${mydist}
+tar -xf ${mytar}
+cd ${blddir} || exit 1
+mytar="cetbuildtools-4.07.03-noarch.tar.bz2"
+mydist="http://scisoft.fnal.gov/scisoft/packages/cetbuildtools/v4_07_03/${mytar}"
+echo "pull ${mytar}"
+curl --fail --silent --location --insecure -O ${mydist}
+tar -xf ${mytar}
+
 echo
 echo "begin build"
 echo
