b18518ed9e2eae911650045d2d4199b46f869439
Eric Flumerfelt <eflumerf@fnal.gov>
2015-10-19 09:33:47 -0500

    Setup the certs/authorized_users file as well.

--- node.js/setupNodeServer.sh
+++ node.js/setupNodeServer.sh
@@ -6,4 +6,5 @@ cd certs
 openssl req -nodes -newkey rsa:2048 -keyout server.key -out server.csr -subj "/C=US/ST=IL/L=Batavia/O=FNAL.GOV/OU=SCD/CN=`hostname`"
 openssl x509 -req -days 365 -in server.csr -signkey server.key -out server.crt
 rm -f server.csr
+echo "`whoami`@FNAL.GOV" >>authorized_users
 cd ..
