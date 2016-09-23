a45cdaa9baf63494dc7d12ef45b70467f33188bd
Eric Flumerfelt <eflumerf@fnal.gov>
2016-09-23 12:21:52 -0500

    Add a build file for artdaq-database

---
+++ for_jenkins/build-artdaq_database.sh
@@ -0,0 +1,131 @@
+#!/bin/bash
+
+# build in $WORKSPACE/build
+# copyback directory is $WORKSPACE/copyBack
+
+usage()
+{
+  cat 1>&2 <<EOF
+Usage: $(basename ${0}) [-h]
+
+Options:
+
+  -h    This help.
+
+EOF
+}
+
+working_dir=${WORKSPACE}
+version=${VERSION}
+qual_set="${QUAL}"
+build_type=${BUILDTYPE}
+demo_version=${ARTDAQ_DEMO_VERSION}
+
+case ${qual_set} in
+    s21:e9)
+	basequal=e9
+	squal=s21
+	;;
+    s34:e10)
+	basequal=e10
+	squal=s34
+	;;
+    s35:e10)
+	basequal=e10
+	squal=s35
+	;;
+	s41:e10)
+	basequal=e10
+	squal=s41
+    ;;
+    *)
+	echo "unexpected qualifier set ${qual_set}"
+	usage
+	exit 1
+esac
+
+case ${build_type} in
+    debug) ;;
+    prof) ;;
+    *)
+	echo "ERROR: build type must be debug or prof"
+	usage
+	exit 1
+esac
+
+dotver=`echo ${version} | sed -e 's/_/./g' | sed -e 's/^v//'`
+
+echo "building the artdaq_demo distribution for ${version} ${dotver} ${qual_set} ${build_type}"
+
+OS=`uname`
+if [ "${OS}" = "Linux" ]
+then
+    flvr=slf`lsb_release -r | sed -e 's/[[:space:]]//g' | cut -f2 -d":" | cut -f1 -d"."`
+elif [ "${OS}" = "Darwin" ]
+then
+  flvr=d`uname -r | cut -f1 -d"."`
+else 
+  echo "ERROR: unrecognized operating system ${OS}"
+  exit 1
+fi
+echo "build flavor is ${flvr}"
+echo ""
+
+qualdir=`echo ${qual_set} | sed -e 's%:%-%'`
+
+set -x
+
+srcdir=${working_dir}/source
+blddir=${working_dir}/build
+# start with clean directories
+rm -rf ${blddir}
+rm -rf ${srcdir}
+rm -rf $WORKSPACE/copyBack 
+# now make the dfirectories
+mkdir -p ${srcdir} || exit 1
+mkdir -p ${blddir} || exit 1
+mkdir -p $WORKSPACE/copyBack || exit 1
+
+cd ${blddir} || exit 1
+curl --fail --silent --location --insecure -O http://scisoft.fnal.gov/scisoft/bundles/tools/pullProducts || exit 1
+chmod +x pullProducts
+
+mv ${blddir}/*source* ${srcdir}/
+
+cd ${blddir} || exit 1
+# pulling binaries is allowed to fail
+# we pull what we can so we don't have to build everything
+./pullProducts ${blddir} ${flvr} artdaq_demo-${demo_version} ${squal}-${basequal} ${build_type}
+
+# Remove any artdaq_database that came with the bundle
+if [ -d ${blddir}/artdaq_database ]; then
+  echo "Removing ${blddir}/artdaq_database"
+  rm -rf ${blddir}/artdaq_database
+fi
+
+echo
+echo "begin build"
+echo
+git clone http://cdcvs.fnal.gov/projects/artdaq-database ${srcdir}/artdaq-database
+cd ${srcdir}/artdaq-database
+git checkout master
+cd ${blddir}
+if [[ "${build_type}" == "prof" ]]; then
+  build_flag="-p"
+else
+  build_flag="-d"
+fi
+source ${srcdir}/artdaq-database/ups/setup_for_development ${build_flag} ${basequal} ${squal}
+buildtool -cpt >build_artdaq-database.log 2>&1 || \
+ { mv ${blddir}/*.log  $WORKSPACE/copyBack/
+   exit 1 
+ }
+
+echo
+echo "move files"
+echo
+mv ${blddir}/*.bz2  $WORKSPACE/copyBack/
+mv ${blddir}/*.txt  $WORKSPACE/copyBack/
+mv ${blddir}/*.log  $WORKSPACE/copyBack/
+
+exit 0
