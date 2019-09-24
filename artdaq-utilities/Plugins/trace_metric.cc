1c8d647e854b934e0d2aeba185f0ba99826768e7
Eric Flumerfelt <eflumerf@fnal.gov>
2019-09-24 09:09:54 -0500

    Run clang-format on trace_metric.cc

--- artdaq-utilities/Plugins/trace_metric.cc
+++ artdaq-utilities/Plugins/trace_metric.cc
@@ -8,21 +8,19 @@
 #include "fhiclcpp/ParameterSet.h"
 #include "trace.h"
 
-
-namespace artdaq
-{
-	/**
+namespace artdaq {
+/**
 	 * \brief TRACEMetric writes metric data to a file on disk
 	 */
-	class TRACEMetric : public MetricPlugin
-	{
-	private:
-		bool stopped_;
-		std::string name_;
-		int lvl_;
+class TRACEMetric : public MetricPlugin
+{
+private:
+	bool stopped_;
+	std::string name_;
+	int lvl_;
 
-	public:
-		/**
+public:
+	/**
 		 * \brief TRACEMetric Constructor. Opens the file and starts the metric
 		 * \param config ParameterSet used to configure TRACEMetric
 		 * \param app_name Name of the application sending metrics
@@ -33,118 +31,119 @@ namespace artdaq
 		 * "trace_level" (Default: TLVL_TRACE): Level to use for metric TRACEs
 		 * \endverbatim
 		 */
-		explicit TRACEMetric(fhicl::ParameterSet const& config, std::string const& app_name) : MetricPlugin(config, app_name)
-			, stopped_(true)
-			, name_(config.get<std::string>("trace_name", app_name + "_TRACEMetric"))
-			, lvl_(config.get<int>("trace_level", TLVL_TRACE))
-		{
-			startMetrics();
-		}
+	explicit TRACEMetric(fhicl::ParameterSet const& config, std::string const& app_name)
+	    : MetricPlugin(config, app_name)
+	    , stopped_(true)
+	    , name_(config.get<std::string>("trace_name", app_name + "_TRACEMetric"))
+	    , lvl_(config.get<int>("trace_level", TLVL_TRACE))
+	{
+		startMetrics();
+	}
 
-		/**
+	/**
 		 * \brief TRACEMetric Destructor. Calls stopMetrics and then closes the file
 		 */
-		virtual ~TRACEMetric()
-		{
-			stopMetrics();
-		}
+	virtual ~TRACEMetric()
+	{
+		stopMetrics();
+	}
 
-		/**
+	/**
 		* \brief Get the library name for the TRACE metric
 		* \return The library name for the TRACE metric, "trace"
 		*/
-		std::string getLibName() const override { return "trace"; }
+	std::string getLibName() const override { return "trace"; }
 
-		/**
+	/**
 		* \brief Write metric data to a file
 		* \param name Name of the metric
 		* \param value Value of the metric
 		* \param unit Units of the metric
 		*/
-		void sendMetric_(const std::string& name, const std::string& value, const std::string& unit) override
+	void sendMetric_(const std::string& name, const std::string& value, const std::string& unit) override
+	{
+		if (!stopped_ && !inhibit_)
 		{
-			if (!stopped_ && !inhibit_)
-			{
-				TLOG(lvl_, name_) << "TRACEMetric: " << name << ": " << value << " " << unit << ".";
-			}
+			TLOG(lvl_, name_) << "TRACEMetric: " << name << ": " << value << " " << unit << ".";
 		}
+	}
 
-		/**
+	/**
 		* \brief Write metric data to a file
 		* \param name Name of the metric
 		* \param value Value of the metric
 		* \param unit Units of the metric
 		*/
-		void sendMetric_(const std::string& name, const int& value, const std::string& unit) override
+	void sendMetric_(const std::string& name, const int& value, const std::string& unit) override
+	{
+		if (!stopped_ && !inhibit_)
 		{
-			if (!stopped_ && !inhibit_)
-			{
-				TLOG(lvl_, name_) << "TRACEMetric: " << name << ": " << value << " " << unit << ".";
-			}
+			TLOG(lvl_, name_) << "TRACEMetric: " << name << ": " << value << " " << unit << ".";
 		}
+	}
 
-		/**
+	/**
 		* \brief Write metric data to a file
 		* \param name Name of the metric
 		* \param value Value of the metric
 		* \param unit Units of the metric
 		*/
-		void sendMetric_(const std::string& name, const double& value, const std::string& unit) override
+	void sendMetric_(const std::string& name, const double& value, const std::string& unit) override
+	{
+		if (!stopped_ && !inhibit_)
 		{
-			if (!stopped_ && !inhibit_)
-			{
-				TLOG(lvl_, name_) << "TRACEMetric: " << name << ": " << value << " " << unit << ".";
-			}
+			TLOG(lvl_, name_) << "TRACEMetric: " << name << ": " << value << " " << unit << ".";
 		}
+	}
 
-		/**
+	/**
 		* \brief Write metric data to a file
 		* \param name Name of the metric
 		* \param value Value of the metric
 		* \param unit Units of the metric
 		*/
-		void sendMetric_(const std::string& name, const float& value, const std::string& unit) override
+	void sendMetric_(const std::string& name, const float& value, const std::string& unit) override
+	{
+		if (!stopped_ && !inhibit_)
 		{
-			if (!stopped_ && !inhibit_)
-			{
-				TLOG(lvl_, name_) << "TRACEMetric: " << name << ": " << value << " " << unit << ".";
-			}
+			TLOG(lvl_, name_) << "TRACEMetric: " << name << ": " << value << " " << unit << ".";
 		}
+	}
 
-		/**
+	/**
 		 * \brief Write metric data to a file
 		 * \param name Name of the metric
 		 * \param value Value of the metric
 		 * \param unit Units of the metric
 		 */
-		void sendMetric_(const std::string& name, const unsigned long int& value, const std::string& unit) override
+	void sendMetric_(const std::string& name, const unsigned long int& value, const std::string& unit) override
+	{
+		if (!stopped_ && !inhibit_)
 		{
-			if (!stopped_ && !inhibit_)
-			{
-				TLOG(lvl_, name_) << "TRACEMetric: " << name << ": " << value << " " << unit << ".";
-			}
+			TLOG(lvl_, name_) << "TRACEMetric: " << name << ": " << value << " " << unit << ".";
 		}
+	}
 
-		/**
+	/**
 		 * \brief Perform startup actions. Writes start message to output file.
 		 */
-		void startMetrics_() override
-		{
-			stopped_ = false;
-			TLOG(TLVL_INFO, name_) << "TRACE Metric Plugin started";
-		}
+	void startMetrics_() override
+	{
+		stopped_ = false;
+		TLOG(TLVL_INFO, name_) << "TRACE Metric Plugin started";
+	}
 
-		/**
+	/**
 		 * \brief Perform shutdown actions. Writes stop message to output file.
 		 */
-		void stopMetrics_() override
-		{
-			stopped_ = true;
-			TLOG(TLVL_INFO, name_) << "TRACE Metric Plugin stopped";
-		}
+	void stopMetrics_() override
+	{
+		stopped_ = true;
+		TLOG(TLVL_INFO, name_) << "TRACE Metric Plugin stopped";
+	}
 
-	private:
-	};
-} //End namespace artdaq
+private:
+};
+}  //End namespace artdaq
 
 DEFINE_ARTDAQ_METRIC(artdaq::TRACEMetric)
