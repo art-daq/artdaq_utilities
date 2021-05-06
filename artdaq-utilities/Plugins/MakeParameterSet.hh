3e8f6d6ba01394d6f102a18fa13a62f4c780032b
Eric Flumerfelt <eflumerf@fnal.gov>
2021-05-06 13:46:34 -0500

    Improvements to MakeParameterSet

--- artdaq-utilities/Plugins/MakeParameterSet.hh
+++ artdaq-utilities/Plugins/MakeParameterSet.hh
@@ -1,3 +1,5 @@
+#ifndef ARTDAQ_MAKE_PARAMETER_SET_HH
+#define ARTDAQ_MAKE_PARAMETER_SET_HH
 
 #include "fhiclcpp/ParameterSet.h"
 #ifndef SIMPLER_PSET_MAKE
@@ -5,7 +7,7 @@
 #endif
 
 namespace artdaq {
-fhicl::ParameterSet make_pset(std::string const& config_str)
+inline fhicl::ParameterSet make_pset(std::string const& config_str)
 {
 #ifdef SIMPLER_PSET_MAKE
 	return fhicl::ParameterSet::make(config_str);
@@ -15,4 +17,17 @@ fhicl::ParameterSet make_pset(std::string const& config_str)
 	return tmp_pset;
 #endif
 }
+
+inline fhicl::ParameterSet make_pset(std::string const& config_file, cet::filepath_maker& maker)
+{
+#ifdef SIMPLER_PSET_MAKE
+	return fhicl::ParameterSet::make(config_file, maker);
+#else
+	fhicl::ParameterSet tmp_pset;
+	fhicl::make_ParameterSet(config_file, maker, tmp_pset);
+	return tmp_pset;
+#endif
+}
 }  // namespace artdaq
+
+#endif  // ARTDAQ_MAKE_PARAMETER_SET_HH
